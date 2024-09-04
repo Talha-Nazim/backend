9b2878ae08d21a4ac4bd490e79ef9be34eec1c80
Talha-Nazim <126316644+Talha-Nazim@users.noreply.github.com>
2024-09-04 14:57:56 +0500

    user registration

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -17,7 +17,8 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type: "auto"
         })
         // file uploaded
-        console.log("file has been uploaded on cloudinary !", response.url);
+        // console.log("file has been uploaded on cloudinary !", response.url);
+        fs.unlinkSync(localFilePath)
         return response
         
     } catch (error) {
